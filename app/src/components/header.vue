01d09f36cf7c655e2f4794554e6b99253921b0a1
Anton Kuklin <antonkuklin99@yandex.ru>
2018-01-22 01:17:46 +0300

    Made buildable and runnable app

--- app/src/components/header.vue
+++ app/src/components/header.vue
@@ -1,5 +1,19 @@
 <template>
+    <div class="header">
+        <router-link class="header-logo">
 
+        </router-link>
+        <div class="header-tools">
+            <ul>
+                <li>
+                    <router-link>Development progress</router-link>
+                </li>
+                <li>
+                    <router-link>Alpha usage</router-link>
+                </li>
+            </ul>
+        </div>
+    </div>
 </template>
 
 <script>
--- app/src/main/main.vue
+++ app/src/main/main.vue
@@ -1,8 +1,15 @@
 <teamplate>
     <div id="main">
+        <v-header></v-header>
     </div>
 </teamplate>
 
 <script>
+    import header from '../components/header.vue'
 
+    export default {
+        components: {
+            'v-header': header
+        }
+    }
 </script>
