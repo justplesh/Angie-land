29edf44acd994f64b6db05a94b8c652d6821b463
Qwerty4Uiop <alexpat930@gmail.com>
2018-01-25 01:30:23 +0300

    Fixed alpha

--- app/src/pages/alpha.vue
+++ app/src/pages/alpha.vue
@@ -1,13 +1,18 @@
 <template>
-    <div>
+    <div class="wrapper">
         <v-header></v-header>
-        <div>
-            <progress-bar type="circle" ref="line" color="#296690"
-                          strokeWidth="0.2" duration="2000"
-            >
-            </progress-bar>
-            <div class="logo-block blue" id="blue">
-                <div class="container">
+        <div class="logo-block blue" id="blue">
+            <div class="container">
+                <div class="side-bar">
+                    <div class="progress-bar">
+                        <progress-bar type="circle" ref="line" color="#296690" strokeWidth="0.2" duration="2000">
+                        </progress-bar>
+                        <p class="score">14/88</p>
+                    </div>
+                </div>
+                <div class="side-text-bar">
+                    <h1>Lorem Ipsum</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                 </div>
             </div>
         </div>
