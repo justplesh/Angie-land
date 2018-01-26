850046c668e26d8dc4daa770c2f521187c53dcbf
Anton Kuklin <antonkuklin99@yandex.ru>
2018-01-26 03:16:53 +0300

    Added new page 'progress', added route to this page, added request to github for stats

---
+++ app/src/pages/progress.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="wrapper">
+        <v-header></v-header>
+        <div class="logo-block">
+            <span>Currently it was made {{ totalCommits }} commits</span>
+        </div>
+    </div>
+</template>
+
+<script>
+    import header from '../components/header.vue'
+
+    const url = 'https://api.github.com/repos/justplesh/Angie-land/stats/contributors';
+
+    export default {
+        name: 'progresses',
+        components: {
+            'v-header': header
+        },
+        data: function () {
+            return {
+                totalCommits: 0
+            }
+        },
+        mounted: function () {
+            this.$http.get(url).then(res => {
+                if (res) {
+                    let totalCommits = 0;
+                    res.body.forEach(function (e) {
+                        totalCommits += e.total;
+                    });
+                    this.totalCommits = totalCommits;
+                    return;
+                }
+                console.log('Something went wrong');
+            })
+        }
+    }
+</script>
