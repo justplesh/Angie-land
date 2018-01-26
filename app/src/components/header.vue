84bd611c0835af7443d64cec9279b5b3ad2449aa
Anton Kuklin <antonkuklin99@yandex.ru>
2018-01-26 03:27:30 +0300

    Added routing in header

--- app/src/components/header.vue
+++ app/src/components/header.vue
@@ -1,15 +1,15 @@
 <template>
-        <header>
-            <div class="logo">
+    <header>
+        <div class="logo">
+            <router-link to="/">
                 <img src="../static/img/cocacola.png" class="center-img"/>
-            </div>
-            <div class="nav">
-                <a href="#">Something 1</a>
-                <a href="#">Something 2</a>
-                <a href="#">Something 3</a>
-                <a href="#">Something 4</a>
-            </div>
-        </header>
+            </router-link>
+        </div>
+        <div class="nav">
+            <router-link to="alpha">Alpha</router-link>
+            <router-link to="progress">Progress</router-link>
+        </div>
+    </header>
 </template>
 
 <script>
