fea66f150fa67191af88d4318d2557fb2909098a
Anton Kuklin <antonkuklin99@yandex.ru>
2018-01-27 00:57:00 +0300

    Made some text's and img's

--- app/src/pages/index.vue
+++ app/src/pages/index.vue
@@ -3,27 +3,33 @@
         <div class="wrapper">
             <v-header></v-header>
             <div class="logo-block red">
-                <img src="../static/img/bottle_PNG2944.png" class="img-block"/>
+                <img src="../static/img/logo.png" class="img-block"/>
             </div>
             <div class="logo-block blue" id="blue">
                 <div class="container">
                     <div class="side-img">
-                        <img src="../static/img/bottle_PNG2944.png" />
+                        <img src="../static/img/hustler.jpg"/>
                     </div>
                     <div class="side-text">
-                        <h1>Lorem Ipsum</h1>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        <h1>Guli-Guli</h1>
+                        <p>
+                            Not another casual messenger, but real application for life. It is necessary as your Iphone X or Tesla. Feel free to text whatever you want, no one will ever see it, only you and your interlocutor. It's absolutely safe and anonymously.
+                        </p>
                     </div>
                 </div>
             </div>
             <div class="logo-block text red" id="red">
                 <div class="container single">
-                    <h1>Lorem Ipsum</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <h1>Why it is so safe?</h1>
+                    <p>
+                    </p>
+                    <p>
+                        Guli-Guli uses partly decentralized network. All information will be passed through our server encoded. It will be encrypted so hard, that neither of KGB, Kommunists or Zloy Russkiy Nikolay could make a sense.
+                    </p>
                 </div>
             </div>
-            <a class="btn-scroll" href="#" v-on:click="changeBtn" v-bind:style="{ backgroundImage: 'url(' + this.directionImg + ')' }" v-scroll-to="this.ref"></a>
+            <a class="btn-scroll" href="#" v-on:click="changeBtn"
+               v-bind:style="{ backgroundImage: 'url(' + this.directionImg + ')' }" v-scroll-to="this.ref"></a>
         </div>
     </div>
 </template>
